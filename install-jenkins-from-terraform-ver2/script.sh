fae0c178dd58bbb203dfdd047cfd34b3d610d8f2
molgan <olga.matsuga@gmail.com>
2021-04-11 19:55:06 +0300

    Update ver2 of install-jenkins-from-terraform-ver2

--- install-jenkins-from-terraform-ver2/script.sh
+++ install-jenkins-from-terraform-ver2/script.sh
@@ -4,11 +4,11 @@
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install openjdk-11-jdk -y
 sudo apt-get update
 sudo add-apt-repository universe
 sudo apt-get install jenkins -y
-sudo apt-get update
-sudo apt-get install openjdk-11-jdk -y
 
 echo "Password to Jenkins "
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
